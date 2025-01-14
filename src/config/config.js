0997baf6348420f0b86d2d61dc19209d888fe894
GMEsparragoza <gianquizard0312@gmail.com>
2025-01-14 15:14:39 -0300

    Prueba de errores

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
 const {
     FRONT_API_URL = 'http://localhost:3000',
     JWT_SECRET = 'access_token',
-    PORT = '3001',
+    PORT = '5000',
     DATABASE_URL = 'postgresql://postgres.rkegjceuqiskoneismhs:Zeyn.expense-tracker@aws-0-sa-east-1.pooler.supabase.com:6543/postgres',
-    IMGUR_ID = ''
+    IMGUR_ID = '',
 } = process.env
 
 module.exports = {
@@ -11,5 +11,5 @@ module.exports = {
     JWT_SECRET,
     PORT,
     DATABASE_URL,
-    IMGUR_ID
+    IMGUR_ID,
 };
