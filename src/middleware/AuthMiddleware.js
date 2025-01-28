4841373951b952e8719790c302abf998f34f41a2
GMEsparragoza <gianquizard0312@gmail.com>
2025-01-28 02:06:57 -0300

    Testeo de cookies en vercel v1

--- src/middleware/AuthMiddleware.js
+++ src/middleware/AuthMiddleware.js
@@ -7,8 +7,9 @@ const verifyToken = (req, res, next) => {
     jwt.verify(token, JWT_SECRET_AUTH, (err, decoded) => {
         if (err) {
             const refreshToken = req.cookies ? req.cookies.refresh_token : null;
-
+            console.log("No se encontro el access token")
             if (!refreshToken) {
+                console.log("No se encontro el token de refresco")
                 return res.status(401).json({ message: 'Debe autenticarse nuevamente' });
             }
 
@@ -16,7 +17,7 @@ const verifyToken = (req, res, next) => {
                 if (err) {
                     return res.status(403).json({ message: 'Refresh Token no válido' });
                 }
-
+                console.log("Existe el token de refresco")
                 const tokenPayload = {
                     id: decodedRefresh.id,
                     email: decodedRefresh.email,
@@ -27,7 +28,7 @@ const verifyToken = (req, res, next) => {
                 const newAccessToken = jwt.sign(tokenPayload, JWT_SECRET_AUTH, { expiresIn: '15m' });
                 // Optional: Refresh the Refresh Token
                 const newRefreshToken = jwt.sign(tokenPayload, JWT_SECRET_REFRESH, { expiresIn: '7d' });
-
+                console.log("Nuevos tokens creados")
                 // Send new tokens in response or set them in cookies
                 res.cookie('access_token', newAccessToken, {
                     httpOnly: true,
@@ -41,11 +42,13 @@ const verifyToken = (req, res, next) => {
                     sameSite: 'None',
                     maxAge: 7 * 24 * 60 * 60 * 1000 // 7 Dias
                 });
+                console.log("Nuevos tokens enviados a las cookies")
 
                 req.user = decodedRefresh; // Optional: Use the refreshed token data
                 next();
             });
         } else {
+            console.log("El usuario esta correctamente autenticado");
             req.user = decoded;
             next();
         }
