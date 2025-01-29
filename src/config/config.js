5f09b207325c50b1ace9b8a59c0652b3234fee88
GMEsparragoza <gianquizard0312@gmail.com>
2025-01-29 18:38:04 -0300

    Correcion de variables v1

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,13 @@
 const {
     FRONT_API_URL = 'http://localhost:3000',
-    JWT_SECRET_AUTH = 'access_token',
-    JWT_SECRET_REFRESH = 'refresh_token',
-    JWT_SECRET_2FA = 'JWT_SECRET_2FA',
+    JWT_SECRET_AUTH = '',
+    JWT_SECRET_REFRESH = '',
+    JWT_SECRET_2FA = '',
     PORT = '5000',
-    DATABASE_URL = 'postgresql://postgres.rkegjceuqiskoneismhs:Zeyn.expense-tracker@aws-0-sa-east-1.pooler.supabase.com:6543/postgres',
-    IMGUR_ID = 'a55a94cfef7bd4f',
-    EMAIL_USER = 'gianm2405@gmail.com',
-    EMAIL_PASS = 'qkgk jioo ivsf wlzp',
+    DATABASE_URL = '',
+    IMGUR_ID = '',
+    EMAIL_USER = '',
+    EMAIL_PASS = '',
 } = process.env
 
 module.exports = {
